91c1ac844266694231c1a5ea3a52149d99a28ee4
Sergei <48413703+Consulus@users.noreply.github.com>
2019-08-16 20:46:41 +0300

    Add files via upload
    change about.html

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
 const menuNav = document.querySelector('.menu-nav');
-const menuBranding = document.querySelector('.menu-branding');
+//const menuBranding = document.querySelector('.menu-branding');
 const navItems = document.querySelectorAll('.nav-item');
 
 // Set Initial State Of Menu
@@ -15,7 +15,7 @@ function toggleMenu() {
         menuBtn.classList.add('close');
         menu.classList.add('show');
         menuNav.classList.add('show');
-        menuBranding.classList.add('show');
+        //menuBranding.classList.add('show');
         navItems.forEach(item => item.classList.add('show'));
 
         //Set Menu State
@@ -24,7 +24,7 @@ function toggleMenu() {
         menuBtn.classList.remove('close');
         menu.classList.remove('show');
         menuNav.classList.remove('show');
-        menuBranding.classList.remove('show');
+        //menuBranding.classList.remove('show');
         navItems.forEach(item => item.classList.remove('show'));
 
         showMenu = false;
@@ -88,7 +88,9 @@ class TypeWriter {
 }
 
 // Init On DOM Load
+
 document.addEventListener('DOMContentLoaded', init);
+
 
 // Init App
 function init() {
